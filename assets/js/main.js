fa72532bec040858d10c51a17cfaa19841c29743
mg45 <m.golabski@gmx.de>
2021-01-18 00:44:16 +0100

    added links for dark-mode - and shadows

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 function switchTheme() {
-    
     let checkBox = document.getElementById("toggleSwitch");
     //console.log(checkBox.checked);
     let body = document.body;
@@ -12,13 +11,22 @@ function switchTheme() {
     let imgSix = document.getElementById('img06');
 
     if (checkBox.checked === true) {
-        // Change to Dark Mode and change img-url- to black
         //console.log("Switch: " + checkBox.checked)
         imgFour.src = "assets/img/darkmode-img4.png"
         imgFive.src = "assets/img/darkmode-img5.png"
         imgSix.src = "assets/img/darkmode-img6.png"
 
+        let linkLight = document.getElementsByTagName('a')
+        var length = linkLight.length;
+        for (let i = 0; i < length; i++) {
+            linkLight[i].className = linkLight[i].className = "link-dark";
+        }
     } else {
+        let linkDark = document.getElementsByTagName('a')
+        var length = linkDark.length;
+        for (let i = 0; i < length; i++) {
+            linkDark[i].className = linkDark[i].className = "link-light";
+        }
         //console.log("Switch: " + checkBox.checked)
         imgFour.src = "assets/img/illustration4.png"
         imgFive.src = "assets/img/illustration5.png"
